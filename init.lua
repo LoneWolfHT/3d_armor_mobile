8be66c53d8a397ddd1fa6add1ca363e686df477c
Lone_Wolf <lonewolf04361@gmail.com>
2018-07-15 14:10:24 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,19 +1,15 @@
-minetest.register_on_joinplayer(function(player)
-local name = player:get_player_name()
-end)
-
 minetest.register_chatcommand("armor", {
-description = "Open Armor Formspec.",
-func = function(name, param)
-minetest.show_formspec(name, "armorform",
-"size[8,5]" ..
-"bgcolor[#080808BB;true]" ..
-"background[5,5;1,1;gui_formbg.png;true]" ..
-"listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]" ..
-"list[detached:".. name .."_armor;armor;1.5,0;5,1;]" ..
-"list[current_player;main;0,1.2;8,1;]" ..
-"list[current_player;main;0,2.35;8,3;8]" ..
-"listring[current_player;main]" ..
-"listring[detached:".. name .."_armor;armor]")
+    description = "Open Armor Formspec.",
+    func = function(name, param)
+        minetest.show_formspec(name, "armorform",
+        "size[8,5]" ..
+        "bgcolor[#080808BB;true]" ..
+        "background[5,5;1,1;gui_formbg.png;true]" ..
+        "listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]" ..
+        "list[detached:".. name .."_armor;armor;1.5,0;5,1;]" ..
+        "list[current_player;main;0,1.2;8,1;]" ..
+        "list[current_player;main;0,2.35;8,3;8]" ..
+        "listring[current_player;main]" ..
+        "listring[detached:".. name .."_armor;armor]")
 end,
 })
